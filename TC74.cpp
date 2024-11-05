f43796ab4d9728173afcc33d02da6f146a9b055e
Ian-Lim-Collab <il356@bath.ac.uk>
2024-11-05 19:30:24 +0000

    add header and cpp files

---
+++ TC74.cpp
@@ -0,0 +1,24 @@
+#include "TC74.h"
+#include <cstdint>
+
+TC74::TC74(int& init_code, PinName sda, PinName scl,uint8_t i2c_7b_addr)
+    :i2c_connection(sda,scl){
+        i2c_addr = i2c_7b_addr << 1;
+        cmd[0] = CHANGE_REG_CMD;
+        cmd[1] = TEMP_REGISTER;
+        init_code = this->i2c_connection.write(this->i2c_addr,cmd,2); // Magic Number is the length of the comamand, 2 wide command
+}
+
+TC74::~TC74(){
+    
+}
+
+int8_t TC74::read(){
+    cmd[0] = READ_CMD;
+    if(this->i2c_connection.write(this->i2c_addr,cmd,1) != 0){ // Magic Number 1 is the lenght of the write data, 1 byte
+        // TODO: Throw error no response error
+    } else {
+        this->i2c_connection.read((int)this->i2c_addr, (char*)this->temp, 1);// Magic Number 1 is the lenght of return data, 1 byte
+    }
+    return (int8_t) *temp;
+}
---
+++ TC74.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "mbed.h"
+#include <cstdint>
+
+class TC74{
+    private:
+        const uint8_t TEMP_REGISTER     = 0x00;
+
+        const uint8_t CHANGE_REG_CMD    = 0x01;
+        const uint8_t READ_CMD          = 0x00;
+        
+    protected:
+        I2C i2c_connection;
+        uint8_t i2c_addr;
+        char cmd[2];
+    public:
+        TC74(int& init_code, PinName sda=I2C_SDA, PinName scl=I2C_SCL,uint8_t i2c_7b_addr = 0b1001101);
+        ~TC74();
+        int8_t read();
+        int8_t* temp;
+};
